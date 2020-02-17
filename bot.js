7de0f04002bd9f9d0b7cca8adfe15e4bd6337b4e
RileyACST <35202521+AjayACST@users.noreply.github.com>
2020-02-18 07:48:59 +1300

    Uploaded Base Code
    Uploaded Base Code and some base commands

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+ const client = new Discord.Client();
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content === 'hello' || msg.content === 'Hello') {
+ msg.reply('Hello!');
+ } else if (msg.content === 'fuck' || msg.content === 'Fuck') {
+    const ayy = client.emojis.find(emoji => emoji.name === "nsfwlemon");
+    msg.reply(`me ${ayy} `);
+ } else if (msg.content === 'dinosaur' || msg.content === 'Dinosaur') {
+    msg.reply('Here have a Dinosaur!')
+    msg.reply('https://media.giphy.com/media/W0QniYnt2WSMrMwDrS/giphy.gif')
+ }
+ });
+
+client.login('Njc4ODE5OTk0MjUwNzcyNDgw.XkooRw.WejqmOp3evx2MgkwZthfchMwJBw');
