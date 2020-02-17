0ccda01a0cc1e11d718d910380336fe35cfee87a
AjayACST <35202521+AjayACST@users.noreply.github.com>
2020-02-18 10:00:51 +1300

    added ahegao
    added ahegao

--- bot.js
+++ bot.js
@@ -5,6 +5,19 @@ client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
+// Put the Music module in the new Client object.
+// This allows for easy access to all the modules
+// functions and data.
+client.music = require("discord.js-musicbot-addon");
+
+
+// Now we start the music module.
+client.music.start(client, {
+   // Set the api key used for YouTube.
+   // This is required to run the bot.
+   youtubeKey: "AIzaSyCfJiyrVCvmJFk2LGQoktNf3XxWNKm0Pvs"
+ });
+
 client.on('message', msg => {
  if (msg.content === 'hello' || msg.content === 'Hello') {
  msg.reply('Hello!');
@@ -14,9 +27,12 @@ client.on('message', msg => {
  } else if (msg.content === 'dinosaur' || msg.content === 'Dinosaur') {
     msg.reply('Here have a Dinosaur!')
     msg.reply('https://media.giphy.com/media/W0QniYnt2WSMrMwDrS/giphy.gif')
- } else if (msg.content === '' || msg.content === '') {
-    
+ } else if (msg.content === 'pussy' || msg.content === 'pussy') {
+   const ahegao = client.emojis.find(emoji => emoji.name === "3932_ahegao");
+   msg.reply(`${ahegao} `);
  }
  });
+
+
 
 client.login('Njc4ODE5OTk0MjUwNzcyNDgw.XkooRw.WejqmOp3evx2MgkwZthfchMwJBw');
