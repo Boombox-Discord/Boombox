7e449c706d9a25c2be2fce7dd740370ebd3ab99f
AjayACST <ajay@acst.co.nz>
2020-10-14 07:54:03 +1300

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const Discord = require('ws');
+const WebSocket = require('ws');
 
 const {
 	prefix,
@@ -16,7 +16,8 @@ const client = new Discord.Client();
 const webSocket = new WebSocket("ws://192.168.178.176:2333/", {
   perMessageDeflate: false,
   headers: {
-    Authorization: `Arrow2080`,
+    Authorization: "Arrow2080",
+    "User-ID": "Test"
   },
 });
 
@@ -53,6 +54,17 @@ client.on('raw', packets => {
     if (!['VOICE_STATE_UPDATE'].includes(packets.t)) return;
     console.log(packets.d.session_id)
     console.log(packets.d.guild_id)
+    let test = {
+      "op": "voiceUpdate",
+      "guildId": packets.d.guild_id,
+      "sessionId": packets.d.session_id
+  }
+  webSocket.on('open', function open() {
+    console.log(test);
+  });
+  webSocket.on('message', function incoming(data) {
+    console.log(data);
+  });
 });
 
 client.login(token);
