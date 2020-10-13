79561884b98afcf340006093afa68e016d8efda9
AjayACST <ajay@acst.co.nz>
2020-10-14 12:14:30 +1300

    Added lots of stuff

--- test.js
+++ test.js
@@ -13,12 +13,21 @@ const {
 const client = new Discord.Client();
 
 
-const webSocket = new WebSocket("ws://192.168.178.176:2333/", {
-  perMessageDeflate: false,
+const webSocket = new WebSocket("ws://123.255.48.162:2333/", {
   headers: {
-    Authorization: "Arrow2080",
-    "User-ID": "Test"
+    "Authorization": "Arrow2080",
+    "Num-Shards": 1,
+    "User-ID": "678819994250772480"
   },
+});
+
+webSocket.on('message', function incoming(data) {
+  console.log(data);
+});
+
+
+webSocket.on('open', function open() {
+  console.log("Connected!");
 });
 
 client.once('ready', () => {
@@ -48,23 +57,43 @@ client.on('message', async msg => {
     const voiceChannel = msg.member.voiceChannel;
     const connection = await voiceChannel.join();
   }
+  if (msg.content.startsWith(`${prefix}play`)) {
+    let play = {
+      "op": "play",
+      "guildId": "592556176009592834",
+      "track": "QAAAqgIAREFub3RoZXIgRGF5IG9mIFN1biAtIExhIExhIExhbmQgKE9yaWdpbmFsIE1vdGlvbiBQaWN0dXJlIFNvdW5kdHJhY2spAAxWw61jdG9yIEh1Z28AAAAAAAN6oAALQ1duWUliMmxxcG8AAQAraHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1DV25ZSWIybHFwbwAHeW91dHViZQAAAAAAAAAA",
+      "noReplace": false
+  };
+
+  webSocket.send(JSON.stringify(play));
+  }
 });
 
+var guildId;
+var sessionId;
+var tokent
+
 client.on('raw', packets => {
-    if (!['VOICE_STATE_UPDATE'].includes(packets.t)) return;
-    console.log(packets.d.session_id)
-    console.log(packets.d.guild_id)
-    let test = {
+    if (!['VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE'].includes(packets.t)) return;
+    if (['VOICE_SERVER_UPDATE'].includes(packets.t)) {
+      var event = packets;
+      tokent = packets.d.token
+    }
+    if (['VOICE_STATE_UPDATE'].includes(packets.t)) {
+      guildId = packets.d.guild_id;
+      sessionId = packets.d.session_id;
+    }
+    console.log(packets)
+    console.log(event)
+    let connect = {
       "op": "voiceUpdate",
-      "guildId": packets.d.guild_id,
-      "sessionId": packets.d.session_id
-  }
-  webSocket.on('open', function open() {
-    console.log(test);
-  });
-  webSocket.on('message', function incoming(data) {
-    console.log(data);
-  });
+      "guildId": guildId,
+      "sessionId": sessionId,
+      "event": event,
+      "token": tokent
+    }
+    console.log(connect)
+    webSocket.send(JSON.stringify(connect));
 });
 
 client.login(token);
