0fb0bd04c84639cc4cda056cb61df76757f93f05
AjayACST <ajay@acst.co.nz>
2021-03-10 19:44:23 +1300

    Added password for redis

--- utils/utils.js
+++ utils/utils.js
@@ -1,11 +1,11 @@
 const redis = require("redis");
 const Lynx = require("lynx");
 
-const { statsdURL, statsdPort, redisIP, redisPort } = require("../config.json"); //skipcq: JS-0266
+const { statsdURL, statsdPort, redisIP, redisPort, redisPassword } = require("../config.json"); //skipcq: JS-0266
 
 var Metrics = new Lynx(statsdURL, statsdPort);
 
-const clientRedis = redis.createClient(redisPort, redisIP, redis);
+const clientRedis = redis.createClient(redisPort, redisIP, redisPassword);
 
 clientRedis.on("error", function (error) {
   console.log("ankglsjndfkg");
